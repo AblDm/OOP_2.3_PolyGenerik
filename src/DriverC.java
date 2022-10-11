76cb5f2aecf40b67a2995746f12256e836ee5c76
Dmabl <sdfghj@dg.jnb>
2022-10-11 12:20:35 +0300

    Домашняя работа Доделка11

--- src/DriverC.java
+++ src/DriverC.java
@@ -1,6 +1,6 @@
 public class DriverC<P> extends DriverAbstract<Bus>{
 
-    public DriverC(String fullName, boolean typeOfLicence, int stage, Bus car, TypeOfCapacity typeOfCapacity) {
+    public DriverC(String fullName, boolean typeOfLicence, int stage, Bus car, TypeOfCapacity capacity ) {
         super (fullName, typeOfLicence, stage, car);
     }
 }
--- src/PassengerHaving.java
+++
@@ -1,7 +0,0 @@
-public class PassengerHaving {
-    boolean passengerHaving;
-
-    public PassengerHaving(boolean passengerHaving) {
-        this.passengerHaving = passengerHaving;
-    }
-}
